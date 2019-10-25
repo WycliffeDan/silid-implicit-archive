018bca1c452ee68fafc88b84371d55d2da0c9b42
Daniel Bidulock <daniel_bidulock@sil.org>
2019-10-25 10:47:13 -0600

    Get new Agent.socialProfile field and email validations

--- src/silid-server/models/agent.js
+++ src/silid-server/models/agent.js
@@ -1,13 +1,25 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const agent = sequelize.define('Agent', {
-    // attributes
     name: {
       type: DataTypes.STRING,
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Agent requires an email'
+        }
+      },
+      unique: {
+        args: true,
+        msg: 'That agent is already registered'
+      }
+    },
+    socialProfile: {
+      strict: true,
+      type: DataTypes.JSONB
     }
   }, {});
 
--- src/silid-server/spec/models/agentSpec.js
+++ src/silid-server/spec/models/agentSpec.js
@@ -1,13 +1,13 @@
 'use strict';
 
-describe('Agent', function() {
+describe('Agent', () => {
   const db = require('../../models');
   const Agent = db.Agent;
 
   let agent;
 
   const _valid = {};
-  beforeEach(function(done) {
+  beforeEach(done => {
     db.sequelize.sync({force: true}).then(() => {
       _valid.name = 'Some Guy';
       _valid.email = 'someguy@example.com';
@@ -20,21 +20,67 @@ describe('Agent', function() {
     });
   });
 
-  describe('basic validation', function() {
+  describe('basic validation', () => {
     const valid = {};
-    beforeEach(function(done) {
+    beforeEach(done => {
        done();
     });
 
-    it('sets the createdAt and updatedAt fields', function(done) {
+    it('sets the createdAt and updatedAt fields', done => {
       expect(agent.createdAt).toBe(undefined);
       expect(agent.updatedAt).toBe(undefined);
-      agent.save().then(function(obj) {
+      agent.save().then(obj => {
         expect(agent.createdAt instanceof Date).toBe(true);
         expect(agent.updatedAt instanceof Date).toBe(true);
         done();
       }).catch(err => {
         done.fail(err);
+      });
+    });
+
+    describe('email', () => {
+      it('requires an email', done => {
+        delete _valid.email;
+        agent = new Agent(_valid);
+        agent.save().then(obj => {
+          done.fail('This shouldn\'t save');
+        }).catch(err => {
+          expect(err.errors.length).toEqual(1);
+          expect(err.errors[0].message).toEqual('Agent requires an email');
+          done();
+        });
+      });
+
+      it('does not allow duplicate emails', done => {
+        agent.save().then(obj => {
+          expect(obj.email).toEqual(_valid.email);
+          let newAgent = new Agent(_valid);
+          newAgent.save().then(obj => {
+            done.fail('This shouldn\'t have saved');
+          }).catch(err => {
+            expect(err.errors.length).toEqual(1);
+            expect(err.errors[0].message).toEqual('That agent is already registered');
+            done();
+          });
+        }).catch(err => {
+          done.fail(err);
+        });
+      });
+    });
+
+    describe('socialProfile', () => {
+      beforeEach(() => {
+        _valid.socialProfile = { provider: 'Google' };
+      });
+
+      it('handles all the stringifying and parsing', function(done) {
+        agent = new Agent(_valid);
+        agent.save().then(obj => {
+          expect(obj.socialProfile).toEqual(_valid.socialProfile);
+          done();
+        }).catch(err => {
+          done.fail(err);
+        });
       });
     });
   });
