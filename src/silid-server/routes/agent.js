4dc355b634cce8372a04a628cafdd7b3529ecc97
Daniel Bidulock <daniel@capitolhill.ca>
2019-10-28 17:00:16 -0600

    Get partial, mostly incomplete CRUD functionality and tests

--- src/silid-server/routes/agent.js
+++ src/silid-server/routes/agent.js
@@ -1,10 +1,56 @@
 const express = require('express');
 const router = express.Router();
 const jwtAuth = require('../lib/jwtAuth');
+const models = require('../models');
 
-/* GET users listing. */
+/* GET agent listing. */
 router.get('/', jwtAuth, function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+router.get('/:id', jwtAuth, function(req, res, next) {
+  models.Agent.findOne({ where: { id: req.params.id } }).then(result => {
+    if (!result) {
+      result = { message: 'No such agent' };
+    }
+    res.json(result);
+  }).catch(err => {
+    res.json(err);
+  });
+});
+
+router.post('/', jwtAuth, function(req, res, next) {
+  let agent = new models.Agent({ email: req.body.email });
+  agent.save().then(result => {
+    res.status(201).json(result);
+  }).catch(err => {
+    res.json(err);
+  });
+});
+
+router.put('/', jwtAuth, function(req, res, next) {
+  models.Agent.findOne({ where: { id: req.body.id } }).then(agent => {
+console.log(agent);
+    if (!agent) {
+      return res.json( { message: 'No such agent' });
+    }
+    for (let key in req.body) {
+      if (agent[key]) {
+        agent[key] = req.body[key];
+      }
+    }
+    agent.update().then(result => {
+console.log('RESULT');
+console.log(result);
+      res.status(201).json(result);
+    }).catch(err => {
+      res.json(err);
+    });
+  }).catch(err => {
+    res.json(err);
+  });
+});
+
 
 module.exports = router;
