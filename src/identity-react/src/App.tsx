876b266bd9c59f236ab439ea9e7b024dfa57c17e
Matthew Reynolds <mattrzr@gmail.com>
2019-11-08 14:15:18 -0600

    Cleanup old codebase, remove spa library

--- src/identity-react/src/App.tsx
+++ src/identity-react/src/App.tsx
@@ -1,41 +1,33 @@
 // src/App.js
 // See: https://auth0.com/docs/quickstart/spa/react/01-login
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import NavBar from './components/NavBar';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import PrivateRoute from './components/PrivateRoute';
-import Profile from './components/Profile';
-import ExternalApi from './components/ExternalApi';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Typography from '@material-ui/core/Typography';
-import { useAuth0 } from './react-auth0-wrapper';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './home/Home';
+import Auth from './auth/Auth';
+import Callback from './callback/Callback';
+import { parseQuery } from './utils/parseQuery';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }),
-);
+const auth = new Auth();
+
+const handleAuthentication = (props: any) => {
+  const { location } = props;
+  if (location.search !== '') {
+    const params = parseQuery(location.search);
+    if (params.inviteId && typeof params.inviteId === 'string') {
+      localStorage.setItem('inviteId', params.inviteId);
+    }
+  }
+  if (/access_token|id_token|error/.test(location.hash)) {
+    auth.handleAuthentication();
+  }
+};
+
 
 function App() {
-  const classes = useStyles();
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
-
 
   return (
     <div className="App">
+<<<<<<< Updated upstream
            <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
@@ -50,16 +42,21 @@ function App() {
           {/* <Button color="inherit">Login</Button> */}
         </Toolbar>
       </AppBar>
+=======
+>>>>>>> Stashed changes
       <BrowserRouter>
-        <header>
-          <NavBar />
-        </header>
-        <Switch>
-          <Route path="/" exact />
-          <PrivateRoute path="/profile" component={Profile} />
-          <PrivateRoute path="/external-api" component={ExternalApi} />
-        </Switch>
-      </BrowserRouter>
+      <Route
+          path="/"
+          render={props => <Home auth={auth} {...props} />}
+        />
+        <Route
+          path="/callback"
+          render={props => {
+            handleAuthentication(props);
+            return <Callback {...props} />;
+          }}
+        />
+        </BrowserRouter>
     </div>
   );
 }
