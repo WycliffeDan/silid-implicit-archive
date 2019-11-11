fbd3f730c7748b3a3a86921473fdd0ef71ec3679
Daniel Bidulock <daniel_bidulock@sil.org>
2019-11-11 18:11:14 -0500

    Remove superfluous code

--- src/identity-react/src/routes/Agent.tsx
+++ src/identity-react/src/routes/Agent.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import Auth from '../auth/Auth';
-import AppBar from '../components/Appbar';
 
-interface IProps  {
-  auth: Auth;
-}
+interface IProps {}
 
 const Agent = (props: IProps) => {
-
-  const { auth } = props;
-
   return (
     <div className="agent">
       <h4>
