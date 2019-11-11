4c36b4465c4949df34148774f99dd1d452a70a24
Daniel Bidulock <daniel_bidulock@sil.org>
2019-11-11 11:39:37 -0700

    Fix indentation

--- src/identity-react/src/routes/Home.tsx
+++ src/identity-react/src/routes/Home.tsx
@@ -5,30 +5,30 @@ import AppBar from '../components/Appbar';
 interface IProps  {
   auth: Auth;
 }
-  const Home = (props: IProps) => {
-    console.log('test');
-    const { auth } = props;
 
-    return (
-      <div className="home">
+const Home = (props: IProps) => {
+  console.log('test');
+  const { auth } = props;
+
+  return (
+    <div className="home">
       <AppBar {...props} />
-        {
-          auth.isAuthenticated() && (
-              <h4>
-                You are logged in!
-              </h4>
-            )
-        }
-        {
-          !auth.isAuthenticated() && (
-              <h4>
-                You are not logged in! Please log in to continue.
-              </h4>
-            )
-        }
-      </div>
-    );
-  }
-
+      {
+        auth.isAuthenticated() && (
+          <h4>
+            You are logged in!
+          </h4>
+        )
+      }
+      {
+        !auth.isAuthenticated() && (
+          <h4>
+            You are not logged in! Please log in to continue.
+          </h4>
+        )
+      }
+    </div>
+  );
+}
 
 export default Home;
