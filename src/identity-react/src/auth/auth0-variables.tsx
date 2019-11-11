c861b69996cd081e38d9055b75b444f6c0edb803
Matthew Reynolds <mattrzr@gmail.com>
2019-11-11 09:35:02 -0600

    Change callback for develop

--- src/identity-react/src/auth/auth0-variables.tsx
+++ src/identity-react/src/auth/auth0-variables.tsx
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'silid.auth0.com',
   clientId: 'tjrl8aOQEx9AtQhFffuWmvP6bcHM7nXB',
-  callbackUrl:'http://localhost:3000/callback',
+  callbackUrl:'https://profile-dev.languagetechnology.org/callback',
   offline: 'true'
 };
