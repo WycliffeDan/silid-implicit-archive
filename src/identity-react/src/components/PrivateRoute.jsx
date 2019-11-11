fc6c3b1a27bafaecbc9afdaa89ce3b4f83f94033
Daniel Bidulock <daniel_bidulock@sil.org>
2019-11-11 16:30:45 -0500

    Ditch Typescript for the moment

--- src/identity-react/src/components/PrivateRoute.jsx
+++ src/identity-react/src/components/PrivateRoute.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import Auth from '../auth/Auth';
 
-interface IProps  {
-  component: React.Component,
-  auth: Auth
-}
-
-const PrivateRoute = ({ component, auth, ...rest }: IProps) => (
+const PrivateRoute = ({ component: Component, auth, ...rest }) => (
   <Route {...rest} render={(props) => (
     auth.isAuthenticated
-      ? <React.Component {...props} />
+      ? <Component {...props} />
       : <Redirect to='/' />
   )} />
 )
