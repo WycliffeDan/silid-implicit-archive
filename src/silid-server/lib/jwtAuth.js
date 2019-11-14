32661f7dd0c1fa0679249fc5e13a37a7a647d2ad
Daniel Bidulock <daniel_bidulock@sil.org>
2019-11-14 14:04:17 -0700

    Clean up

--- src/silid-server/lib/jwtAuth.js
+++ src/silid-server/lib/jwtAuth.js
@@ -46,33 +46,6 @@ const jwtAuth = function(req, res, next) {
   }).catch(err => {
     res.status(500).json(err);
   });
-  
-//  if (!req.agent) {
-//    return res.status(401).json({ message: 'Unauthorized: Invalid token' });
-//  }
-//  const token =
-//    req.body.token ||
-//    req.query.token ||
-//    req.headers['x-access-token'] ||
-//    req.cookies.token;
-//  if (!token) {
-//    res.status(401).json({ message: 'Unauthorized: No token provided' });
-//  } else {
-//    jwt.verify(token, process.env.CLIENT_SECRET, function(err, decoded) {
-//      if (err) {
-//        res.status(401).json({ message: 'Unauthorized: Invalid token' });
-//      } else {
-//        models.Agent.findOne({ where: { email: decoded.email } }).then(agent => {
-//          req.user = agent;
-//          if (!req.user) {
-//            req.user = decoded;
-//          }
-//          next();
-//        }).catch(err => {
-//          res.status(500).json(err);
-//        });
-//      }
-//    });
-//  }
 }
+
 module.exports = jwtAuth;
