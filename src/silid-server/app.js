7c0bef8d1d535a959cc90d32a91b23e1f0cc2bd4
Daniel Bidulock <daniel_bidulock@sil.org>
2019-11-15 13:25:15 -0700

    Implement jwks-rsa token check

--- src/silid-server/app.js
+++ src/silid-server/app.js
@@ -12,6 +12,8 @@ var organizationRouter = require('./routes/organization');
 var teamRouter = require('./routes/team');
 
 const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
 
 var app = express();
 
@@ -28,7 +30,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 /**
  * Access Token verification
  */
-app.use(jwt({ secret: process.env.CLIENT_SECRET, requestProperty: 'agent' }));
+//app.use(jwt({ secret: process.env.CLIENT_SECRET, requestProperty: 'agent' }));
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  requestProperty: 'agent',
+  algorithm: ["RS256"]
+});
+
+app.use(checkJwt);
 
 /**
  * Routes
