3972afe8ad099bd54743d893df119f2bc8beb743
Daniel Bidulock <daniel_bidulock@sil.org>
2019-11-25 18:42:54 -0500

    Get stdout log task

--- src/identity-react/cypress/plugins/index.js
+++ src/identity-react/cypress/plugins/index.js
@@ -2,4 +2,19 @@ const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
 
 module.exports = on => {
   on('file:preprocessor', cypressTypeScriptPreprocessor)
+
+
+  /**
+   * 2019-11-25 https://stackoverflow.com/questions/52070262/cypress-pipe-console-log-and-command-log-to-output
+   *
+   * Log to stdout in headless tests
+   */
+  on('task', {
+    log (message) {
+      console.log(message);
+      return null;
+    }
+  });
 }
+
+
