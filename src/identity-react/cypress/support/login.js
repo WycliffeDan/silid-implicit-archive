d1eed57fe5ee61224e014dae490a171c65138d9d
Daniel Bidulock <daniel_bidulock@sil.org>
2019-12-04 14:46:57 -0700

    Cheat the profile picture loading

--- src/identity-react/cypress/support/login.js
+++ src/identity-react/cypress/support/login.js
@@ -7,6 +7,8 @@ Cypress.Commands.add('login', function(token) {
     name: 'loginViaAuth0',
   });
 
+  cy.fixture('google-profile-response.json').as('profile');
+
   cy.request({ url: 'http://localhost:3002/sign', method: 'POST', body: { accessToken: token } }).then(function(res) {
 
     cy.visit('/', {
@@ -15,7 +17,9 @@ Cypress.Commands.add('login', function(token) {
         win.localStorage.setItem('accessToken', res.body);
         win.localStorage.setItem('idToken', 'id-token-here');
         win.localStorage.setItem('expiresAt', Math.floor(new Date()) + 3600);
-//        win.localStorage.setItem('profile', JSON.stringify(profile));
+        // This is a total cheat. This data is being loaded in fixtures rather
+        // than going to the extreme length of mocking the `idTokenPayload` 
+        win.localStorage.setItem('profile', JSON.stringify(this.profile));
       },
     });
   });
