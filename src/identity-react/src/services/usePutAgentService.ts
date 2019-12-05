e59ec0b0aba673a29390cc8b67a19c9d58adc3ce
Daniel Bidulock <daniel_bidulock@sil.org>
2019-12-04 17:56:21 -0700

    Hello, usePutAgentService

---
+++ src/identity-react/src/services/usePutAgentService.ts
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { Service } from '../types/Service';
+import { Agent } from '../types/Agent';
+
+export type PutAgent = Pick<Agent, 'name' | 'email' | 'id'>;
+
+const usePutAgentService = () => {
+  const [service, setService] = useState<Service<PutAgent>>({
+    status: 'init',
+  });
+
+  //const publishAgent = (agent: PutAgent) => {
+  const publishAgent = (agent: any) => {
+    setService({ status: 'loading' });
+
+    const headers = new Headers();
+//    const headers = new Headers({ 'Authorization': `Bearer ${localStorage.getItem('accessToken')}`}); 
+    headers.append('Authorization', `Bearer ${localStorage.getItem('accessToken')}`);
+    headers.append('Content-Type', 'application/json; charset=utf-8');
+
+    return new Promise((resolve, reject) => {
+      fetch('/agent',
+        {
+          method: 'PUT',
+          body: JSON.stringify(agent),
+          headers,
+        }
+      )
+        .then(response => response.json())
+        .then(response => {
+          setService({ status: 'loaded', payload: response });
+          resolve(response);
+        })
+        .catch(error => {
+          setService({ status: 'error', error });
+          reject(error);
+        });
+    });
+  };
+
+  return {
+    service,
+    publishAgent,
+  };
+};
+
+export default usePutAgentService;
