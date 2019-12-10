3beafbd842ff9a95a4643daeffddb722b2257991
Daniel Bidulock <daniel_bidulock@sil.org>
2019-12-10 10:28:15 -0700

    Hello, useGetOrganizationInfoService

---
+++ src/identity-react/src/services/useGetOrganizationInfoService.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import { Service } from '../types/Service';
+import { Organization } from '../types/Organization';
+
+const useOrganizationInfoService = (id: number) => {
+  const [result, setResult] = useState<Service<Organization>>({
+    status: 'loading'
+  });
+
+
+  const headers = new Headers({ 'Authorization': `Bearer ${localStorage.getItem('accessToken')}`}); 
+  useEffect(() => {
+    fetch(`/organization/${id}`, { headers })
+      .then(response => response.json())
+      .then(response => {
+        if (response.message) {
+          console.log('SUCCESS');
+          console.log(response);
+          setResult({ status: 'error', error: response.message })
+        }
+        else {
+          setResult({ status: 'loaded', payload: response })
+        }
+      })
+      .catch(error => setResult({ status: 'error', error }));
+  }, []);
+
+  return result;
+};
+
+export default useOrganizationInfoService;
