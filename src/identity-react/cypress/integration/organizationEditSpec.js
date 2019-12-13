31feb022848810e5e91371c6fd5263a46013cb58
Daniel Bidulock <daniel_bidulock@sil.org>
2019-12-13 14:26:04 -0700

    Hello, organizationEditSpec

---
+++ src/identity-react/cypress/integration/organizationEditSpec.js
@@ -0,0 +1,132 @@
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="Cypress" />
+
+context('Organization edit', function() {
+
+  before(function() {
+    cy.fixture('someguy-auth0-access-token.json').as('agent');
+  });
+
+  afterEach(() => {
+    cy.task('query', 'TRUNCATE TABLE "Organizations" CASCADE;');
+  });
+  
+  describe('Editing', () => {
+
+    let token, agent, organization;
+    beforeEach(function() {
+      cy.login(this.agent);
+      cy.visit('/#/').then(() => {
+        token = localStorage.getItem('accessToken');
+        cy.task('query', `SELECT * FROM "Agents" WHERE "accessToken"='Bearer ${token}' LIMIT 1;`).then(([results, metadata]) => {
+          agent = results[0];
+  
+          cy.request({ url: '/organization',  method: 'POST', auth: { bearer: token }, body: { name: 'One Book Canada' } }).then((org) => {
+            organization = org.body;
+            cy.get('#app-menu-button').click();
+            cy.get('#organization-button').click();
+            cy.contains('One Book Canada').click();
+          });
+        });
+      });
+    });
+
+    describe('Edit button', () => {
+      it('displays the edit form', () => {
+        cy.get('form#edit-organization-form').should('not.exist');
+        cy.get('button#edit-organization').click();
+        cy.get('form#edit-organization-form').should('exist');
+      });
+  
+      it('displays the organization info in form', function() {
+        cy.get('button#edit-organization').click();
+        cy.get('input[name="name"][type="text"]').should('have.value', organization.name);
+        cy.get('button[type="submit"]').should('exist');
+      });
+    });
+
+    describe('Cancel button', () => {
+      beforeEach(() => {
+        cy.get('button#edit-organization').click();
+      });
+
+      it('displays on form change', () => {
+        cy.get('input[name="name"][type="text"]').should('have.value', organization.name);
+        cy.get('button#cancel-changes').should('not.exist');
+        cy.get('input[name="name"][type="text"]').clear().type('Two Testaments Canada');
+        cy.get('button#cancel-changes').should('exist');
+      });
+
+      it('resets changes to the form', () => {
+        cy.get('input[name="name"][type="text"]').should('have.value', organization.name);
+        cy.get('input[name="name"][type="text"]').clear().type('Two Testaments Canada');
+        cy.get('button#cancel-changes').click();
+        cy.get('button#edit-organization').click();
+        cy.get('input[name="name"][type="text"]').should('have.value', organization.name);
+      });
+
+      it('hides on click', () => {
+        cy.get('input[name="name"][type="text"]').clear().type('Two Testaments Canada');
+        cy.get('button#cancel-changes').should('exist');
+        cy.get('button#cancel-changes').click();
+        cy.get('button#cancel-changes').should('not.exist');
+      });
+
+      it('hides form on click', () => {
+        cy.get('input[name="name"][type="text"]').clear().type('Two Testaments Canada');
+        cy.get('button#cancel-changes').click();
+        cy.get('form#edit-organization-form').should('not.exist');
+      });
+    });
+
+    describe('unsuccessful changes', () => {
+      beforeEach(() => {
+        cy.get('button#edit-organization').click();
+      });
+
+
+      it('does not allow an empty field', () => {
+        cy.get('input[name="name"][type="text"]:invalid').should('have.length', 0)
+        cy.get('input[name="name"][type="text"]').should('have.value', organization.name);
+        cy.get('input[name="name"][type="text"]').clear();
+        cy.get('input[name="name"][type="text"]').should('have.value', '');
+        cy.get('button[type="submit"]').click();
+        cy.get('input[name="name"][type="text"]:invalid').should('have.length', 1)
+        cy.get('input[name="name"][type="text"]:invalid').then($input => {
+          expect($input[0].validationMessage).to.eq('name required')
+        });
+      });
+    });
+
+    describe('successful changes', () => {
+      beforeEach(() => {
+        cy.get('button#edit-organization').click();
+      });
+
+      it('updates the record in the database', function() {
+        cy.task('query', `SELECT * FROM "Organizations" WHERE "id"='${organization.id}' LIMIT 1;`).then(([results, metadata]) => {
+          cy.get('input[name="name"][type="text"]').should('have.value', organization.name);
+          cy.get('input[name="name"][type="text"]').clear().type('Two Testaments Canada');
+          cy.get('button[type="submit"]').click();
+          cy.wait(500);
+          cy.task('query', `SELECT * FROM "Organizations" WHERE "id"='${organization.id}' LIMIT 1;`).then(([results, metadata]) => {
+            expect(results[0].name).to.eq('Two Testaments Canada');
+          });
+        });
+      });
+
+      it('updates the record on the interface', function() {
+        cy.get('h3').contains(organization.name);
+        cy.get('input[name="name"][type="text"]').should('have.value', organization.name);
+        cy.get('input[name="name"][type="text"]').clear().type('Two Testaments Canada');
+        cy.get('button[type="submit"]').click();
+        cy.get('h3').contains('Two Testaments Canada');
+        cy.get('button#edit-organization').click();
+        cy.get('input[name="name"][type="text"]').should('have.value', 'Two Testaments Canada');
+      });
+    });
+  });
+});
+
+export {}
