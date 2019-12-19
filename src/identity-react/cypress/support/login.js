df096ab4c499b67ab76747770add1c9ef9444863
Daniel Bidulock <daniel_bidulock@sil.org>
2019-12-19 15:31:15 -0700

    Get an identity token

--- src/identity-react/cypress/support/login.js
+++ src/identity-react/cypress/support/login.js
@@ -7,20 +7,26 @@ Cypress.Commands.add('login', function(token) {
     name: 'loginViaAuth0',
   });
 
+  // Sign an access token
   cy.request({ url: 'http://localhost:3002/sign', method: 'POST', body: { accessToken: token } }).then(function(res) {
     const signedToken = res.body;
 
-    // Access with a token creates an agent the database
-    cy.request({ url: '/agent',  method: 'GET', auth: { bearer: signedToken } }).then(function(res) {
+    // Sign an identity token
+    cy.request({ url: 'http://localhost:3002/sign', method: 'POST', body: { accessToken: { iss: 'https://silid.auth0.com/'} } }).then(function(res) {
+      const idToken = res.body;
 
-      cy.visit('/', {
-        onBeforeLoad: (win) => {
-          win.localStorage.setItem('isLoggedIn', 'true');
-          win.localStorage.setItem('accessToken', signedToken);
-          win.localStorage.setItem('idToken', 'id-token-here');
-          win.localStorage.setItem('expiresAt', Math.floor(new Date()) + 3600);
-          win.localStorage.setItem('profile', JSON.stringify(res.body.socialProfile));
-        },
+      // Access with a token creates an agent the database
+      cy.request({ url: '/agent',  method: 'GET', auth: { bearer: signedToken } }).then(function(res) {
+
+        cy.visit('/', {
+          onBeforeLoad: (win) => {
+            win.localStorage.setItem('isLoggedIn', 'true');
+            win.localStorage.setItem('accessToken', signedToken);
+            win.localStorage.setItem('idToken', idToken);
+            win.localStorage.setItem('expiresAt', Math.floor(new Date()) + 3600);
+            win.localStorage.setItem('profile', JSON.stringify(res.body.socialProfile));
+          },
+        });
       });
     });
   });
