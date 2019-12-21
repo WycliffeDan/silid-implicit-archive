b5ad4f770bb4de3927d0fab68b492757cda8bdf1
Daniel Bidulock <daniel_bidulock@sil.org>
2019-12-21 10:37:40 -0700

    Fix flaky test

--- src/identity-react/cypress/integration/organizationDeleteSpec.js
+++ src/identity-react/cypress/integration/organizationDeleteSpec.js
@@ -128,9 +128,10 @@ context('Organization delete', function() {
           });
           cy.task('query', `SELECT * FROM "Organizations";`).then(([results, metadata]) => {
             expect(results.length).to.eq(1);
-            cy.get('button#delete-organization').click();
-            cy.task('query', `SELECT * FROM "Organizations";`).then(([results, metadata]) => {
-              expect(results.length).to.eq(0);
+            cy.get('button#delete-organization').click().then(() => {
+              cy.task('query', `SELECT * FROM "Organizations";`).then(([results, metadata]) => {
+                expect(results.length).to.eq(0);
+              });
             });
           });
         });
