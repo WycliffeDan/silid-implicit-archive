6bb82043ccfd519e518609dc5a007e2fa90f3d05
Daniel Bidulock <daniel_bidulock@sil.org>
2019-12-21 10:31:09 -0700

    Comment out some hard-coded values

--- src/identity-react/src/auth/auth0-variables.tsx
+++ src/identity-react/src/auth/auth0-variables.tsx
@@ -1,6 +1,8 @@
 export const AUTH_CONFIG = {
-  domain: 'silid.auth0.com',
-  clientId: 'tjrl8aOQEx9AtQhFffuWmvP6bcHM7nXB',
+  //domain: 'silid.auth0.com',
+  domain: process.env.REACT_APP_DOMAIN,
+  //clientId: 'tjrl8aOQEx9AtQhFffuWmvP6bcHM7nXB',
+  clientId: process.env.REACT_APP_CLIENT_ID,
   //callbackUrl:'https://profile-dev.languagetechnology.org/callback',
   callbackUrl: process.env.REACT_APP_CALLBACK_URL,
   audience: 'https://id.languagetechnology.org/',
