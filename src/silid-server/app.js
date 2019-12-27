b193e4e3bac0ea7b226d2669edf20b6c7ff93484
Daniel Bidulock <daniel_bidulock@sil.org>
2019-12-27 08:08:50 -0700

    Configure server to serve client build in production and staging

--- src/silid-server/app.js
+++ src/silid-server/app.js
@@ -32,7 +32,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static(path.join(__dirname, 'build')));
+}
+else {
+  app.use(express.static(path.join(__dirname, 'public')));
+}
 
 /**
  * Access Token verification
