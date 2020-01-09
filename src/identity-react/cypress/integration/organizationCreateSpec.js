576f7251fc2baa6243e9cda380cf82e486132b70
Daniel Bidulock <daniel_bidulock@sil.org>
2020-01-09 14:13:57 -0700

    Test for field focus and blank entries

--- src/identity-react/cypress/integration/organizationCreateSpec.js
+++ src/identity-react/cypress/integration/organizationCreateSpec.js
@@ -7,7 +7,7 @@ context('Organization creation', function() {
   before(function() {
     cy.fixture('someguy-auth0-access-token.json').as('agent');
   });
-  
+
   let token, agent;
 
   context('authenticated', () => {
@@ -21,15 +21,15 @@ context('Organization creation', function() {
         });
       });
     });
-  
+
     afterEach(() => {
       cy.task('query', 'TRUNCATE TABLE "Organizations" CASCADE;');
     });
-  
+
     it('lands in the right spot', () => {
       cy.url().should('contain', '/#/organization');
     });
-  
+
     describe('interface', () => {
       it('displays common Organization interface elements', () => {
         cy.get('h3').contains('Organizations');
@@ -44,6 +44,12 @@ context('Organization creation', function() {
           cy.get('form#add-organization-form').should('not.exist');
           cy.get('button#add-organization').click();
           cy.get('form#add-organization-form').should('exist');
+        });
+
+        it('yields focus to the first form field', () => {
+          cy.get('form#add-organization-form').should('not.exist');
+          cy.get('button#add-organization').click();
+          cy.focused().should('have.attr', 'name').and('eq', 'name');
         });
 
         describe('add-organization-form', () => {
@@ -64,7 +70,7 @@ context('Organization creation', function() {
               cy.get('button#cancel-changes').click();
               cy.get('form#add-organization-form').should('not.exist');
             });
-  
+
             it('clears the name input field', function() {
               cy.get('input[name="name"][type="text"]').type('The Justice League');
               cy.get('button#cancel-changes').click();
@@ -74,42 +80,53 @@ context('Organization creation', function() {
           });
 
           describe('add-organization-button', () => {
-            it('does not allow an empty field', function() {
-              cy.get('input[name="name"][type="text"]').clear();
-              cy.get('input[name="name"][type="text"]').should('have.value', '');
-              cy.get('#add-organization-button').click();
-              cy.get('input[name="name"][type="text"]:invalid').should('have.length', 1)
-              cy.get('input[name="name"][type="text"]:invalid').then($input => {
-                expect($input[0].validationMessage).to.eq('name required')
+            context('valid form', () => {
+              it('updates the record in the database', function() {
+                cy.task('query', `SELECT * FROM "Organizations";`).then(([results, metadata]) => {
+                  expect(results.length).to.eq(0);
+                  cy.get('input[name="name"][type="text"]').type('The Justice League');
+                  cy.get('button[type="submit"]').click();
+                  cy.wait(500);
+                  cy.task('query', `SELECT * FROM "Organizations"`).then(([results, metadata]) => {;
+                    expect(results[0].name).to.eq('The Justice League');
+                  });
+                });
               });
-            });
-      
-            it('updates the record in the database', function() {
-              cy.task('query', `SELECT * FROM "Organizations";`).then(([results, metadata]) => {
-                expect(results.length).to.eq(0);
+
+              it('hides the add-organization-form', function() {
                 cy.get('input[name="name"][type="text"]').type('The Justice League');
                 cy.get('button[type="submit"]').click();
                 cy.wait(500);
-                cy.task('query', `SELECT * FROM "Organizations"`).then(([results, metadata]) => {;
-                  expect(results[0].name).to.eq('The Justice League');
-                });
+                cy.get('form#add-organization-form').should('not.exist');
+              });
+
+              it('updates the record on the interface', function() {
+                cy.get('#organization-list').should('not.exist');
+                cy.get('input[name="name"][type="text"]').type('The Justice League');
+                cy.get('button[type="submit"]').click();
+                cy.wait(500);
+                cy.get('#organization-list').find('.organization-list-item').its('length').should('eq', 1);
+                cy.get('#organization-list .organization-list-item').first().contains('The Justice League');
               });
             });
-      
-            it('hides the add-organization-form', function() {
-              cy.get('input[name="name"][type="text"]').type('The Justice League');
-              cy.get('button[type="submit"]').click();
-              cy.wait(500);
-              cy.get('form#add-organization-form').should('not.exist');
-            });
 
-            it('updates the record on the interface', function() {
-              cy.get('#organization-list').should('not.exist');
-              cy.get('input[name="name"][type="text"]').type('The Justice League');
-              cy.get('button[type="submit"]').click();
-              cy.wait(500);
-              cy.get('#organization-list').find('.organization-list-item').its('length').should('eq', 1);
-              cy.get('#organization-list .organization-list-item').first().contains('The Justice League');
+            context('invalid form', () => {
+              describe('name field', () => {
+                it('does not allow an empty field', function() {
+                  cy.get('input[name="name"][type="text"]').type('International Association of Super Criminals');
+                  cy.get('input[name="name"][type="text"]').clear();
+                  cy.get('input[name="name"][type="text"]').should('have.value', '');
+                  cy.get('.error').contains('This is a required field');
+                  cy.get('button[type="submit"]').should('be.disabled');
+                });
+
+                it('does not allow a blank field', function() {
+                  cy.get('input[name="name"][type="text"]').type('     ');
+                  cy.get('input[name="name"][type="text"]').should('have.value', '     ');
+                  cy.get('.error').contains('name can\'t be blank');
+                  cy.get('button[type="submit"]').should('be.disabled');
+                });
+              });
             });
           });
         });
